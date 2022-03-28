14507ed6ce186f756bbfdc4e8a818e9d67c987f1
Adam.Harin <Adam.Harin@quest.com>
2022-03-28 12:10:32 +0200

    Icon

--- WebApi/ClientApp/src/components/elements/Logo.tsx
+++ WebApi/ClientApp/src/components/elements/Logo.tsx
@@ -1,17 +1,10 @@
 import { Flex, Text, useBreakpointValue, Image, ImageProps } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../contextProviders/AuthProvider'
+import icon from '../../img/icon.png'
 
 export const Icon = (props: ImageProps) => {
-  return (
-    <Image
-      {...props}
-      height={8}
-      viewBox='0 0 120 28'
-      src='http://cdn.onlinewebfonts.com/svg/img_533278.png'
-      filter='invert(83%) sepia(9%) saturate(2030%) hue-rotate(86deg) brightness(90%) contrast(88%)'
-    />
-  )
+  return <Image {...props} height={8} viewBox='0 0 120 28' src={icon} />
 }
 
 export const Logo = () => {
