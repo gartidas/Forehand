ce0b1b2c3243280ec355ddb9f99e0bb8f525453b
Adam.Harin <Adam.Harin@quest.com>
2022-03-30 08:39:37 +0200

    DeleteOverdueSubscriptionCardsWorker

---
+++ WebApi/BackgroundWorkes/DeleteOverdueSubscriptionCardsWorker.cs
@@ -0,0 +1,71 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NCrontab;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WebApi.Features.SubscriptionCards;
+
+namespace WebApi.BackgroundWorkes
+{
+    public class DeleteOverdueSubscriptionCardsWorker : BackgroundService
+    {
+        private CrontabSchedule _schedule;
+        private DateTime _nextRun;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<DeleteOverdueSubscriptionCardsWorker> _logger;
+
+        private string Schedule => "0 0 23 * * *"; //Runs every day at 23:00
+
+        public DeleteOverdueSubscriptionCardsWorker(IServiceProvider serviceProvider, ILogger<DeleteOverdueSubscriptionCardsWorker> logger)
+        {
+            _logger = logger;
+            _serviceProvider = serviceProvider;
+            _schedule = CrontabSchedule.Parse(Schedule, new CrontabSchedule.ParseOptions { IncludingSeconds = true });
+            _nextRun = _schedule.GetNextOccurrence(DateTime.UtcNow);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("DeleteOverdueSubscriptionCards starting...");
+
+            try
+            {
+                do
+                {
+                    if (DateTime.UtcNow > _nextRun)
+                    {
+                        try
+                        {
+                            using (var scope = _serviceProvider.CreateScope())
+                            {
+                                var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+                                await mediator.Send(new DeleteOverdueSubscriptionCards.Command(), stoppingToken);
+                                _logger.LogInformation("DeleteOverdueSubscriptionCards executed at {0}", DateTime.UtcNow);
+                            }
+                        }
+                        catch (OperationCanceledException) when (stoppingToken.IsCancellationRequested)
+                        {
+                            // Pass
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex, "Error occured during DeleteOverdueSubscriptionCards execution.");
+                        }
+
+                        _nextRun = _schedule.GetNextOccurrence(DateTime.UtcNow);
+                    }
+
+                    await Task.Delay(TimeSpan.FromMinutes(20), stoppingToken);
+                }
+                while (!stoppingToken.IsCancellationRequested);
+            }
+            finally
+            {
+                _logger.LogDebug("DeleteOverdueSubscriptionCards stopped.");
+            }
+        }
+    }
+}
---
+++ WebApi/Features/SubscriptionCards/DeleteOverdueSubscriptionCards.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebApi.Persistence;
+
+namespace WebApi.Features.SubscriptionCards
+{
+    public class DeleteOverdueSubscriptionCards
+    {
+        public class Command : IRequest<Unit>
+        {
+        }
+
+        public class Handler : IRequestHandler<Command, Unit>
+        {
+            private readonly ForehandContext _db;
+
+            public Handler(ForehandContext db)
+            {
+                _db = db;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var currentDate = DateTime.Now;
+                var subscriptionCards = await _db.SubscriptionCards.Where(x => x.DueDate < currentDate).ToListAsync(cancellationToken);
+
+                _db.SubscriptionCards.RemoveRange(subscriptionCards);
+                await _db.SaveChangesAsync(cancellationToken);
+                return Unit.Value;
+            }
+        }
+    }
+}
